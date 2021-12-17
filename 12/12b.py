8744213f8cebe636fa2f5ef566c7b601c2623365
Chris <cmg5461@gmail.com>
2021-12-17 09:41:46 -0500

    problem 12 fix

--- 12/12b.py
+++ 12/12b.py
@@ -1,24 +1,34 @@
+import csv
+
 import numpy as np
 import pandas as pd
 
 from typing import List, Tuple, Dict
 
 
-class Problem12a:
+class Problem12b:
     def __init__(self):
         self.node_dict: Dict[str, str] = {}
         self.count = 0
 
-    def get_steps(self, node="start", node_path=[]):
+    def get_steps(self, node="start", node_path=[], twice=False):
         nptemp = node_path.copy() + [node]
         if node == "end":
+            self.count += 1
+            print(self.count)
             return 1
 
         nodes: str = self.node_dict[node]
         count = 0
         for n in nodes:
-            if n.isupper() or n not in node_path:
-                count += self.get_steps(node=n, node_path=nptemp)
+            if twice and n.islower() and n in node_path:
+                continue
+            if n.isupper() or node_path.count(n) < 2:
+                count += self.get_steps(
+                    node=n,
+                    node_path=nptemp,
+                    twice=twice or (n.islower() and node_path.count(n) == 1)
+                )
         return count
 
     def solve(self):
@@ -34,14 +44,18 @@ class Problem12a:
             if node2 not in self.node_dict:
                 self.node_dict[node2] = []
 
-            self.node_dict[node1].append(node2)
-            self.node_dict[node2].append(node1)
+            if node2 != "start":
+                self.node_dict[node1].append(node2)
+            if node1 != "start":
+                self.node_dict[node2].append(node1)
 
         steps = self.get_steps(node="start")
-
-        print(f"Solution to 12a: {steps}")
+        print(f"Solution to 12b: {steps}")
 
 
 if __name__ == '__main__':
-    problem = Problem12a()
+    import time
+    problem = Problem12b()
+    start_time = time.time()
     problem.solve()
+    print("--- %s seconds ---" % (time.time() - start_time))
